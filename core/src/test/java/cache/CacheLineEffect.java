adf30b71b30880a4c5ae93577948a291e805c5d8
zhengye.zhang <zhengye.zhang@owitho.com>
2018-05-18 14:48:59 +0800

    FalseSharing

--- core/src/test/java/cache/CacheLineEffect.java
+++ core/src/test/java/cache/CacheLineEffect.java
@@ -1,9 +1,21 @@
 package cache;
 
+import sun.misc.Contended;
+
 /**
  * @Author: zhengye.zhang
  * @Description:
  * @Date: 2018/5/18 下午2:15
+ * <p>
+ * Cache是由很多个cache line组成的。每个cache line通常是64字节，并且它有效地引用主内存中的一块儿地址。
+ * <p>
+ * 一个Java的long类型变量是8字节，因此在一个缓存行中可以存8个long类型的变量。
+ * <p>
+ * CPU每次从主存中拉取数据时，会把相邻的数据也存入同一个cache line。
+ * <p>
+ * 在访问一个long数组的时候，如果数组中的一个值被加载到缓存中，它会自动加载另外7个。
+ * <p>
+ * 因此你能非常快的遍历这个数组。事实上，你可以非常快速的遍历在连续内存块中分配的任意数据结构。
  */
 public class CacheLineEffect {
     //考虑一般缓存行大小是64字节，一个 long 类型占8字节

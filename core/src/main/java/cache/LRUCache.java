a6d87d92eadc1b0f68a7de8a6878905d2d6ade1b
modong@pinduoduo.com <zhengye.zhang@owitho.com>
2018-08-22 20:08:46 +0800

    lru cache

---
+++ core/src/main/java/cache/LRUCache.java
@@ -0,0 +1,65 @@
+package cache;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @author modong
+ * @date 2018/8/22 下午5:09
+ */
+public class LRUCache<K, V> {
+    /**
+     * 最大缓存大小
+     */
+    private int maxCacheSize;
+
+    private LinkedHashMap<K, V> cache;
+
+    public LRUCache(int maxCacheSize) {
+        this.maxCacheSize = maxCacheSize;
+        this.cache = new LinkedHashMap<K, V>() {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry eldest) {
+                return cache.size() == maxCacheSize + 1;
+            }
+        };
+    }
+
+
+    public V get(K k) {
+        return cache.get(k);
+    }
+
+    public V getOrDefault(K key, V defaultValue) {
+        return cache.getOrDefault(key, defaultValue);
+    }
+
+    public V put(K key, V value) {
+        return cache.put(key, value);
+    }
+
+    public boolean containsKey(K key) {
+        return cache.containsKey(key);
+    }
+
+    public void clear() {
+        cache.clear();
+    }
+
+    public V remove(K key) {
+        return cache.remove(key);
+    }
+
+    public int size() {
+        return cache.size();
+    }
+
+    public boolean isEmpty() {
+        return cache.isEmpty();
+    }
+
+    public boolean containsValue(V value) {
+        return cache.containsValue(value);
+    }
+
+}
---
+++ core/src/test/java/cache/LRUCacheTest.java
@@ -0,0 +1,31 @@
+package cache;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author modong
+ * @date 2018/8/22 下午7:47
+ */
+public class LRUCacheTest {
+
+
+    @Test
+    public void test() throws Exception {
+        LRUCache<String, Integer> lruCache = new LRUCache<>(5);
+
+        lruCache.put("a", 1);
+        lruCache.put("b", 2);
+        lruCache.put("c", 3);
+        lruCache.put("d", 4);
+        lruCache.put("e", 5);
+        Assert.assertTrue(1 == lruCache.get("a"));
+        Assert.assertEquals(5, lruCache.size());
+
+        //触发lru
+        lruCache.put("f", 6);
+        Assert.assertEquals(5, lruCache.size());
+
+        Assert.assertNull(lruCache.get("a"));
+    }
+}
